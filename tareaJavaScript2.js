95e2516cd8ea2ddf829e4a0ff12ea10a0afd810f
Carlos Reges <kerg23@gmail.com>
2022-10-07 13:22:15 -0300

    Segunda tarea de JavaScript en Openbootcamp

---
+++ tareaJavaScript2.js
@@ -0,0 +1,15 @@
+const nombre = "Carlos Reges";
+let edad = 41;
+const soyDesarrollador = true;
+const fechaNacimiento = new Date(1981, 7, 26);
+const libroFavorito = {
+    título: "Cien años de soledad",
+    autor: "Gabriel García Márquez",
+    fecha: 1967,
+    url: "https://es.wikipedia.org/wiki/Cien_a%C3%B1os_de_soledad"
+}
+
+console.log(nombre);
+console.log(edad);
+console.log(fechaNacimiento);
+console.log(libroFavorito.título);
